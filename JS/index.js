b673455c7e16cfc24a0d7d14363b83fc21d1d085
030didi <a17489800@gmail.com>
2024-10-31 23:27:34 +0800

    排版問題

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const changeText = () => {
-        document.getElementById('line9b').textContent = 'PSYCHO';
-    };
-    setTimeout(changeText, 13000); // 13秒後改變文字
-});
-
-
 jQuery(document).ready(function ($) {
     $("li.content").hide();
     $("ul.toggle-menu").delegate("li.toggle", "click", function () {
