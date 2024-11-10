5e5ce682a4c999edb76bc0c2b6b802225c34a0f1
030didi <a17489800@gmail.com>
2024-11-10 22:47:14 +0800

    finish

--- JS/index.js
+++ JS/index.js
@@ -46,3 +46,94 @@ document.addEventListener("DOMContentLoaded", function () {
     animateProgress(90, 'progress2', 'circle2');
     animateProgress(100, 'progress3', 'circle3');
 });
+
+
+// 為三個不同的幻燈片設置控制器
+let slideIndex1 = 1;
+let slideIndex2 = 1;
+let slideIndex3 = 1;
+
+// 第一個幻燈片控制器
+function showSlides1(props) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlide1");
+
+    if (props > slides.length) {
+        slideIndex1 = 1;
+    }
+
+    if (props < 1) {
+        slideIndex1 = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slides[slideIndex1 - 1]) {
+        slides[slideIndex1 - 1].style.display = "flex";
+    }
+}
+
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
+}
+
+// 第二個幻燈片控制器
+function showSlides2(props) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlide2");
+
+    if (props > slides.length) {
+        slideIndex2 = 1;
+    }
+
+    if (props < 1) {
+        slideIndex2 = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slides[slideIndex2 - 1]) {
+        slides[slideIndex2 - 1].style.display = "flex";
+    }
+}
+
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+}
+
+// 第三個幻燈片控制器
+function showSlides3(props) {
+    let i;
+    let slides = document.getElementsByClassName("singleSlide3");
+
+    if (props > slides.length) {
+        slideIndex3 = 1;
+    }
+
+    if (props < 1) {
+        slideIndex3 = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slides[slideIndex3 - 1]) {
+        slides[slideIndex3 - 1].style.display = "flex";
+    }
+}
+
+function plusSlides3(n) {
+    showSlides3(slideIndex3 += n);
+}
+
+// 初始化所有幻燈片
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides1(slideIndex1);
+    showSlides2(slideIndex2);
+    showSlides3(slideIndex3);
+});
