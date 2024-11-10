040c8a1067afd38422697b4de452c4ff2ab14ce3
030didi <a17489800@gmail.com>
2024-11-10 11:27:03 +0800

    修好jQurey 、初始動畫以及下面文字跑版

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const changeText = () => {
+        document.getElementById('line9b').textContent = 'Fish'; // Change the text for line 9b
+
+    };
+
+    // Change the text after 15 seconds
+    setTimeout(changeText, 13000);
+});
+
+
 jQuery(document).ready(function ($) {
     $("li.content").hide();
     $("ul.toggle-menu").delegate("li.toggle", "click", function () {
